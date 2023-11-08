b736dd9d3272eee9ff33589ec4eca5ac069ac6ee
Hugo Reyes <hugofer300@gmail.com>
2023-11-07 22:48:56 -0500

    Trying config changes

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,3 +1,2 @@
-export const PAGE_URL =
-  "https://hugh140.github.io/tree_spoti/";
+export const PAGE_URL = "https://hugh140.github.io/tree_spoti/";
 export const clientId = "175032166c78487884952133cf50d3d7";
