a6d21fd9b951ef60b2d763ab42b4db23f2119439
hugh140 <hugofer300@gmail.com>
2023-11-08 16:52:59 -0500

    Fixing deploy errors

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,4 +1,4 @@
 export const PAGE_URL = 
-  // "https://hugh140.github.io/tree_spoti/";
-  "http://localhost:4000/"
+  "https://hugh140.github.io/tree_spoti/";
+  // "http://localhost:4000/"
 export const clientId = "175032166c78487884952133cf50d3d7";
